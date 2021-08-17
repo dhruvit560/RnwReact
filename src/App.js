f1bd87781eb2a9514c9ebc368c07fe66af6a1043
dhruvitpatel <dhruvitpatel@knovator.in>
2021-08-17 21:09:30 +0530

    table into data

--- src/App.js
+++ src/App.js
@@ -2,23 +2,70 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+        let data = [
+          {
+              name: "Dhruvit",
+              age: 21,
+              salary: 200000,
+              bonus: 150000,
+              status: true
+          },
+          {
+              name: "Jenish",
+              age: 25,
+              salary: 400000,
+              bonus: 350000,
+              status: false
+          },
+          {
+              name: "Darshak",
+              age: 23,
+              salary: 5000,
+              bonus: 400,
+              status: true
+          },
+          {
+              name: "Dharmesh",
+              age: 28,
+              salary: 30000,
+              bonus: 600,
+              status: true,
+          },
+          {
+              name: "Pinki",
+              age: 33,
+              salary: 6000,
+              bonus: 1000,
+              status: true
+          }
+  ]	
+  
+  let tbody = document.getElementById("dataHere");
+		
+		data.map((val,index)=>{
+			var tr = document.createElement('tr');	
+			for(i in val){
+				var td = document.createElement('td');
+				td.innerHTML = val[i];
+				tr.appendChild(td);	
+			}
+			tbody.appendChild(tr);
+		});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <table id="myTbl" width="100%" border="1" cellpadding="10" cellspacing="0" style="border-collapse:collapse;">
+        <thead>
+          <tr>				
+            <th>Name</th>
+            <th>Age</th>
+            <th>Salary</th>
+            <th>Bonus</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody id="dataHere"></tbody>
+      </table>
+    </>
   );
 }
 
